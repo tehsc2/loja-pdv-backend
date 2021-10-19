72402dcd6ee326d234b0e18b4f3370428c04b5da
Esther <esther.souza@ab-inbev.com>
2021-10-18 22:08:16 -0300

    Cancelar um pedido

--- src/main/java/com/pdv/pdv/controllers/PedidoController.java
+++ src/main/java/com/pdv/pdv/controllers/PedidoController.java
@@ -2,13 +2,14 @@ package com.pdv.pdv.controllers;
 
 import com.pdv.pdv.entities.Pedido;
 import com.pdv.pdv.entities.Produto;
+import com.pdv.pdv.entities.enums.StatusPedidoEnum;
 import com.pdv.pdv.repositories.PedidoRepository;
 import com.pdv.pdv.repositories.ProdutoRepository;
 import com.pdv.pdv.utils.ErrorMessage;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +24,7 @@ public class PedidoController {
     private final ProdutoRepository produtoRepository;
 
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
-    public ResponseEntity<?> post(@RequestBody Pedido pedido) {
+    public ResponseEntity<?> createUptadePedido(@RequestBody Pedido pedido) {
         List<Long> idsProduto = pedido.getItensPedido().stream().map(itemPedido -> itemPedido.getProduto().getId()).collect(Collectors.toList());
         List<Produto> produtos = produtoRepository.findAllById(idsProduto);
         pedido.getItensPedido().forEach(itemPedido -> {
@@ -46,5 +47,18 @@ public class PedidoController {
         pedido.removeProdutosEstoque();
         pedidoRepository.save(pedido);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{idPedido}")
+    public ResponseEntity<?> cancelarPedido(@PathVariable Long idPedido){
+        Pedido pedido = pedidoRepository.findById(idPedido).orElse(null);
+
+        if (pedido != null){
+            pedido.setStatusPedido(StatusPedidoEnum.CANCELADO);
+            pedidoRepository.save(pedido);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage("Pedido não encontrado", "ID PEDIDO: " + idPedido));
     }
 }
