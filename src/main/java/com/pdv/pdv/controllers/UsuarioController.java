3e06d390a1d8411a6334351df8b186fb4fbef0d4
Esther <esther.souza@ab-inbev.com>
2021-10-25 22:26:29 -0300

    Ajustes login

--- src/main/java/com/pdv/pdv/controllers/UsuarioController.java
+++ src/main/java/com/pdv/pdv/controllers/UsuarioController.java
@@ -4,8 +4,11 @@ import com.pdv.pdv.entities.Usuario;
 import com.pdv.pdv.repositories.UsuarioRepository;
 import com.pdv.pdv.utils.ErrorMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +21,17 @@ public class UsuarioController {
     @GetMapping("/{idUsuario}")
     public ResponseEntity<?> getUsuario(@PathVariable Long idUsuario) {
         return ResponseEntity.ok(usuarioRepository.findById(idUsuario));
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<?> validaLogin(@PathParam(value = "login") String login, @PathParam(value = "senha") String senha) {
+        Usuario usuario = usuarioRepository.findByUsuarioAndSenha(login, senha);
+
+        if (usuario != null) {
+            return ResponseEntity.ok().body(usuario);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
--- src/main/java/com/pdv/pdv/repositories/UsuarioRepository.java
+++ src/main/java/com/pdv/pdv/repositories/UsuarioRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Usuario findByUsuarioAndSenha(String login, String senha);
 }
